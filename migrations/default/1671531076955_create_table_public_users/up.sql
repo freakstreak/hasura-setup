7959d948fccce566e4b856a634a98602b9958202
Tanmay Jaiswal <tanmayj@geekyants.com>
2022-12-20 17:31:20 +0530

    inita

---
+++ migrations/default/1671531076955_create_table_public_users/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."users" ("id" serial NOT NULL, "firstName" text NOT NULL, "lastName" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, PRIMARY KEY ("id") );
---
+++ migrations/default/1671531125561_create_table_public_blogs/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."blogs" ("id" serial NOT NULL, "title" text NOT NULL, "body" text NOT NULL, "userId" integer NOT NULL, PRIMARY KEY ("id") );
